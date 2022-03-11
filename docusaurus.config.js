125e99ddb5ce19a1b7d7313af1c7ad9f20cf8f35
Laurent Lao <lao.laurent@gmail.com>
2022-03-11 15:22:45 -0500

    #1 Configure deployment branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'laurent-lao', // Usually your GitHub org/user name.
   projectName: 'laurent-lao.github.io', // Usually your repo name.
+  deploymentBranch: 'main',
 
   presets: [
     [
