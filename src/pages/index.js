c1acdacd0ec0b82912ec6c1b64b5cb5ecd1773e6
Laurent Lao <lao.laurent@gmail.com>
2022-03-12 15:29:58 -0500

    #1 Revamp styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,21 +5,29 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Typewriter from "typewriter-effect";
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        {/* <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div> */}
+        <h1 className="hero__title">
+        <Typewriter
+          options={{
+            cursor: '',
+            delay: 70,
+          }}
+          onInit={(typewriter) => {
+            typewriter.typeString(
+              'Hi, I\'m Laurent.'
+              +'<br/>'
+              +'This is my website.'
+            )
+            .start();
+          }}
+        />
+        </h1>
       </div>
     </header>
   );
