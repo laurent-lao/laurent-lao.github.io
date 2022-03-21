c1050391a202956142a072b431b028c0b52f9003
Laurent Lao <32573725+laurent-lao@users.noreply.github.com>
2022-03-21 18:33:56 -0400

    Update Favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -87,7 +87,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Home page`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
